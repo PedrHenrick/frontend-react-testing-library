dbe79fc571312d9d7d5893f151039291af787162
Pedro Henrick <phmc@ic.ufal.br>
2022-02-18 19:47:09 -0300

    Requisito 4 Completo: Testando a pagina not found, texto e imagem

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+describe('4. Teste o componente `<NotFound.js />`', () => {
+  test('Teste se página contém um `h2` com o texto `Page requested not found 😭`', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+
+    customHistory.push('/notFound');
+
+    const notFoundEl = screen.getByRole('heading', {
+      name: /Page requested not found/i, level: 2 });
+    const emoji = screen.getByText('😭');
+
+    expect(notFoundEl && emoji).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+
+    customHistory.push('/notFound');
+
+    const imgEl = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imgEl.src).toContain(url);
+  });
+});

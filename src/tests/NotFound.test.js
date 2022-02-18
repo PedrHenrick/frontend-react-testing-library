b95fb8e337c5ace0a70df780e1af47cfcfccd853
Pedro Henrick <phmc@ic.ufal.br>
2022-02-18 19:49:02 -0300

    Corrigindo erros do lint #1

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createMemoryHistory } from 'history';
-import { MemoryRouter, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 
